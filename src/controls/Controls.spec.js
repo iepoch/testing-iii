ad7d75b01cc53036b5e60bd943b26d54b60b76d0
iepoch <staclem@live.com>
2019-02-04 22:14:23 -0700

    Running into issue with fireing the button and not getting the open gate...

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,12 +2,32 @@
 // Test away!
 import React from "react";
 import Controls from "./Controls";
-import { render } from "react-testing-library";
+import { render, fireEvent } from "react-testing-library";
 import "jest-dom/extend-expect";
+// import waitForExpect from "wait-for-expect";
+
+// afterEach(cleanup);
 
 describe("should Controls the gate", () => {
   it("should render the Controls", () => {
     const div = document.createElement("div");
     render(<Controls />, div);
+  });
+
+  it("should render buttons closed and locked", () => {
+    const { getByText } = render(<Controls closed={false} locked={false} />);
+    getByText(/lock gate/i);
+    getByText(/close gate/i);
+  });
+
+  it("should display open button when closed is clicked",  () => {
+    const { getByText } = render(<Controls closed={false} locked={false} />);
+    const closeBtn = getByText(/close gate/i);
+      fireEvent.click(closeBtn);
+      const openBtn = getByText(/open gate/i)
+
+    // expect(openBtn).toHaveTextContent(/open gate/i)
+
+
   });
 });
